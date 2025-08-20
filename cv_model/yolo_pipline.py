e8c2de715ebd88e8923dcd26f07498a61b90c79b
siparau <ivansiparov795@gmail.com>
2025-08-20 15:21:24 +0300

    Детектим боксы, которые находятся между двумя линиями

--- cv_model/yolo_pipline.py
+++ cv_model/yolo_pipline.py
@@ -6,12 +6,18 @@ from tqdm import tqdm
 class YOLOBagDetector:
     def __init__(self, yolo_weights_path='best.pt', device="cpu"):
         self.model = YOLO(yolo_weights_path).to(device)
+        self.left_line = [0, 604, 380, 246]
+        self.right_line = [735, 275, 596, 645]
+
+    def _is_center_between_lines(self, cx, cy):
+        def is_left_of_line(x, y, line):
+            x1, y1, x2, y2 = line
+            return (x2 - x1) * (y - y1) - (y2 - y1) * (x - x1) > 0
+
+        return is_left_of_line(cx, cy, self.left_line) and not is_left_of_line(cx, cy, self.right_line)
     
     def _is_center_inside_box(self, cx, cy, box):
         return box[0] < cx < box[2] and box[1] < cy < box[3]
-    
-
-            
 
     def predict(self, video_path, n_frames=2, conf=0.5):
         cap = cv2.VideoCapture(video_path)
@@ -44,15 +50,16 @@ class YOLOBagDetector:
                         if results.boxes.id is not None and len(results.boxes.id) > 0:
                             bag_list = []
                             for box, track_id in zip(results.boxes.xyxy, results.boxes.id):
-                                #unique_bag.add(int(track_id))
                                 x1, y1, x2, y2 = map(int, box.tolist())
-                                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                                cv2.putText(frame, f"bag {int(track_id)}", (x1, y1 - 5),
-                                            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-                                bag_list.append([
-                                    track_id,
-                                    [x1, y1, x2, y2]
-                                ])
+                                cx, cy = (x1 + x2) / 2, (y1 + y2) / 2
+                                if self._is_center_between_lines(cx, cy):
+                                    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                                    cv2.putText(frame, f"bag {int(track_id)}", (x1, y1 - 5),
+                                                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+                                    bag_list.append([
+                                        track_id,
+                                        [x1, y1, x2, y2]
+                                    ])
                             
                             bags_to_add = []
 
@@ -62,11 +69,10 @@ class YOLOBagDetector:
                                 cy = (box[1] + box[3]) / 2
                                 is_nested = False
                                 for other_bag in bag_list:
-                                    for other_bag in bag_list:
-                                        if bag[0] != other_bag[0]:
-                                            if self._is_center_inside_box(cx, cy, other_bag[1]):
-                                                is_nested = True
-                                                break
+                                    if bag[0] != other_bag[0]:
+                                        if self._is_center_inside_box(cx, cy, other_bag[1]):
+                                            is_nested = True
+                                            break
                                 
                                 if not is_nested and track_id not in unique_bag:
                                     bags_to_add.append(track_id)
